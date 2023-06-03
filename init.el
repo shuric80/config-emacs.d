ec4183145e735c849fc0c83228f1dcfa5128c461
Aleksandr Batenov <shuric80@gmail.com>
2023-06-03 15:46:03 +0300

    added for python development

--- init.el
+++ init.el
@@ -112,3 +112,145 @@
   :hook (python-mode . (lambda ()
                          (require 'lsp-pyright)
                          (lsp))))  ; or lsp-deferred
+
+
+(use-package pyvenv
+  :demand t
+  :config
+  (setq pyvenv-workon "emacs")  ; Default venv
+  (pyvenv-tracking-mode 1))
+
+
+;;GIT
+
+(use-package git-gutter
+ :demand t
+ :init
+ (progn
+   (defun md/set-sensible-column ()
+     "Unless file is too big, either git-gutter mode (when in git dir)"
+     (interactive)
+     (when (and (< (count-lines (point-min) (point-max)) 1500)
+                (not (eq major-mode 'org-mode)))
+       (if (string= "git" (downcase (format "%s" (vc-backend
+                                                  (buffer-file-name
+                                                   (current-buffer))))))
+           (git-gutter-mode 1))))
+   (add-hook 'find-file-hook 'md/set-sensible-column))
+
+
+ :config
+ (progn
+   (setq git-gutter:ask-p nil  ; Don't ask for confirmation of gadd
+         git-gutter:modified-sign "~"
+         git-gutter:added-sign "+"
+         git-gutter:deleted-sign "-"
+
+         ;; This ensures the separator is always displayed
+         git-gutter:unchanged-sign " "
+         git-gutter:always-show-separator t
+
+         ;; Without this, there's no space between the git-gutter column and the code.
+         git-gutter:separator-sign " "))
+ :bind (:map md/leader-map
+       ("g <RET>" . git-gutter-mode)
+       ("gk" . git-gutter:previous-hunk)
+       ("gp" . git-gutter:previous-hunk)
+       ("gj" . git-gutter:next-hunk)
+       ("gn" . git-gutter:next-hunk)
+       ("g+" . git-gutter:stage-hunk)
+       ("g-" . git-gutter:revert-hunk)))
+
+(use-package magit
+ :demand t
+ :config
+ (progn
+   (evil-set-initial-state 'magit-blame-mode 'normal)
+   (evil-set-initial-state 'magit-revision-mode 'normal)
+   (evil-set-initial-state 'magit-diff-mode 'normal)
+   (evil-set-initial-state 'magit-status-mode 'normal)
+
+   (add-hook 'magit-diff-mode 'evil-normal-state)
+   (add-hook 'magit-status-mode 'evil-normal-state)
+
+   (defun md/magit-quit ()
+     (interactive)
+     (magit-mode-bury-buffer)
+     (shackle--eyebrowse-close-slot-by-tag "git"))
+
+   (evil-define-key 'normal magit-mode-map
+     (kbd "TAB") 'magit-section-toggle
+     (kbd "<RET>") 'magit-visit-thing
+     "q" 'md/magit-quit
+     "r" 'magit-refresh
+     "n" 'magit-section-forward
+     "p" 'magit-section-backward
+     "+" 'magit-stage-file
+     "-" 'magit-unstage-file
+     "[" 'magit-diff-less-context
+     "]" 'magit-diff-more-context
+     )
+
+   (evil-define-key 'emacs magit-log-mode-map
+     "q" 'md/magit-quit)
+
+   ;;(setq magit-display-buffer-function 'magit-display-buffer-fullframe-status-v1)
+   (setq magit-display-buffer-function 'display-buffer)
+
+   ;; I don't know why, but by default I can't get magit-blame to adhere to my
+   ;; normal-mode map below, even though Evil says I'm in normal mode. Explicitly
+   ;; calling evil-normal-state fixes it.
+   (add-hook 'magit-blame-mode-hook 'evil-normal-state)
+   (evil-define-key 'normal magit-blame-mode-map
+     (kbd "<RET>") 'magit-show-commit
+     "q" 'magit-blame-quit
+     "gj" 'magit-blame-next-chunk
+     "gn" 'magit-blame-next-chunk
+     "gk" 'magit-blame-previous-chunk
+     "gp" 'magit-blame-previous-chunk)
+
+   (add-hook 'magit-revision-mode-hook 'evil-normal-state)
+   (evil-define-key 'normal magit-revision-mode-map
+     (kbd "<RET>") 'magit-diff-visit-file
+     "q" 'magit-mode-bury-buffer))  ;; This quits
+
+ :bind (:map md/leader-map
+       ("gg" . magit-status)
+       ("gm" . magit-dispatch-popup)
+       ("gb" . magit-blame)
+       ("gl" . magit-log-head)
+
+       ;; Diff gives the full git diff output. Ediff shows ediff for a single
+       ;; file.
+       ("gd" . magit-diff-buffer-file)
+       ("gD" . magit-diff-dwim)
+       ("ge" . magit-ediff-popup)
+
+       ;; NOTE - this doesn't play nicely with mode-line:
+       ;; - https://github.com/magit/magit/blob/master/Documentation/magit.org#the-mode-line-information-isnt-always-up-to-date
+       ;; - https://github.com/syl20bnr/spacemacs/issues/2172
+       ("gC" . magit-commit-popup)
+       ("gc" . magit-checkout)))
+;;YAML
+(use-package yaml-mode :demand t)
+
+;;MARKDOWN
+(use-package markdown-mode
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.gfm\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.apib\\'" . markdown-mode)  ; Apiary
+         ("\\.markdown\\'" . markdown-mode))
+  :config (progn
+            ;; Markdown-cycle behaves like org-cycle, but by default is only
+            ;; enabled in insert mode. gfm-mode-map inherits from
+            ;; markdown-mode-map, so this will enable it in both.
+            (evil-define-key 'normal markdown-mode-map
+              (kbd "TAB") 'markdown-cycle
+              "gk" 'markdown-previous-visible-heading
+              "gj" 'markdown-next-visible-heading)))
+
+
+(use-package dockerfile-mode)
+
